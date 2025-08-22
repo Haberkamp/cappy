9181d21b4580cbe78affd52adb9c6ce1ccaf552f
Nils Haberkamp <haberkamp.n@gmail.com>
2025-08-22 21:24:08 +0200

    chore: text content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <p>Hello, world</p>;
+  return (
+    <div className="container max-w-[980px] mx-auto pt-[180px]">
+      <h1 className="font-semibold capped-text-heading tracking-tighter text-neutral-1200">
+        Upload video. Download captions.
+      </h1>
+
+      <div className="pt-6"></div>
+
+      <p className="font-semibold capped-text-subheading tracking-tight text-neutral-900">
+        Transcribe and create captions for your videos.
+      </p>
+    </div>
+  );
 }
