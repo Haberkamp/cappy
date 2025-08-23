421eff887119bf7412aae43e19f1746a9ed67b36
Nils Haberkamp <haberkamp.n@gmail.com>
2025-08-23 13:54:01 +0200

    chore: add upload box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,7 @@ import { motion } from "motion/react";
 export default function Home() {
   return (
     <div className="container px-4 max-w-[980px] mx-auto pt-[180px]">
-      <motion.h1
-        className="font-semibold capped-text-heading tracking-tighter text-neutral-1200"
-        style={{ width: "max-content" }}
-      >
+      <motion.h1 className="font-semibold capped-text-heading tracking-tighter text-neutral-1200 w-auto">
         <motion.span
           initial={{ opacity: 0, filter: "blur(4px)", scale: 0.98 }}
           animate={{ opacity: 1, filter: "blur(0px)", scale: 1 }}
@@ -33,11 +30,50 @@ export default function Home() {
         initial={{ opacity: 0, filter: "blur(4px)", scale: 0.99 }}
         animate={{ opacity: 1, filter: "blur(0px)", scale: 1 }}
         transition={{ duration: 0.5, delay: 1 }}
-        className="font-semibold capped-text-subheading tracking-tight text-neutral-900"
-        style={{ width: "max-content" }}
+        className="font-semibold capped-text-subheading tracking-tight text-neutral-900 w-auto text-balance"
       >
         Transcribe and create captions for your videos.
       </motion.p>
+
+      <div className="pt-16"></div>
+
+      <div
+        className="border border-neutral-600 w-full min-h-[400px] rounded-2xl bg-neutral-100 grid place-items-center px-8"
+        style={{ boxShadow: "0 0 7px 0 rgba(0, 0, 0, 0.05)" }}
+      >
+        <div className="flex flex-col items-center">
+          <p className="text-center font-semibold capped-text-subheading text-neutral-1200 tracking-tight text-balance">
+            Drag and drop a file to create captions
+          </p>
+
+          <div className="pt-4"></div>
+
+          <p className="text-center capped-text-body text-neutral-900 text-balance">
+            You can upload a .mp3, .mp4, .wav and .mov file.
+          </p>
+
+          <div className="pt-6"></div>
+
+          <button
+            className="text-white bg-black flex items-center gap-2 px-6 min-h-11 rounded-lg focus-visible:outline-accent-900 outline-offset-2 outline-2 cursor-pointer select-none"
+            type="button"
+          >
+            <svg
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+              style={{ width: "18px", height: "18px" }}
+            >
+              <path d="M224,144v64a8,8,0,0,1-8,8H40a8,8,0,0,1-8-8V144a8,8,0,0,1,16,0v56H208V144a8,8,0,0,1,16,0ZM93.66,77.66,120,51.31V144a8,8,0,0,0,16,0V51.31l26.34,26.35a8,8,0,0,0,11.32-11.32l-40-40a8,8,0,0,0-11.32,0l-40,40A8,8,0,0,0,93.66,77.66Z"></path>
+            </svg>
+
+            <span>Upload file</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
