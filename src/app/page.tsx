1ffb80c3e85fbff8f2f4b44a79587c4cfb34c493
Nils Haberkamp <haberkamp.n@gmail.com>
2025-08-23 22:01:01 +0200

    fix: adjust color of information bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -186,7 +186,7 @@ export default function Home() {
             damping: 19.8,
             stiffness: 400,
           }}
-          className="flex items-center h-[36px] z-10 absolute left-1/2 -translate-x-1/2 top-[100%] w-9/10 bg-neutral-400 border border-t-0 border-neutral-600 rounded-b-lg py-3 px-3 flex items-center justify-between"
+          className="flex items-center h-[36px] z-10 absolute left-1/2 -translate-x-1/2 top-[100%] w-9/10 bg-neutral-300 border border-t-0 border-neutral-600 rounded-b-lg py-3 px-3 flex items-center justify-between"
           style={{ boxShadow: "0 2px 3px rgba(0, 0, 0, 0.05)" }}
         >
           {typeof downloadProgress === "number" ? (
